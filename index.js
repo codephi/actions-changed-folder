87bec5a1201b86899a1d50450f784fdbcb347fb9
Philippe Assis <codephilippe@gmail.com>
2023-06-17 10:49:31 -0300

    list paths

--- index.js
+++ index.js
@@ -1,40 +1,46 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 async function run() {
-  try {
-    const pathToCheck = core.getInput('path');
+    try {
+        const foldersToCheck = JSON.parse(core.getInput('folders'));
+
+        const changedFolders = await Promise.all(foldersToCheck.map(async (folder) => {
+            const isFolderChanged = await checkIfFolderChanged(folder.name, folder.directory);
+            return {
+                name: folder.name,
+                changed: isFolderChanged
+            };
+        }));
+
+        core.setOutput('changed', JSON.stringify(changedFolders));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+async function checkIfFolderChanged(name, directory) {
     const changedFiles = await getChangedFiles();
 
-    const isPathChanged = changedFiles.includes(pathToCheck);
-
-    if (isPathChanged) {
-      core.setOutput('changed', 'true');
-      console.log(`The path "${pathToCheck}" was changed.`);
-    } else {
-      core.setOutput('changed', 'false');
-      console.log(`The path "${pathToCheck}" was not changed.`);
-    }
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    return changedFiles.some(file => file.startsWith(`${directory}/`));
 }
 
 async function getChangedFiles() {
-  const token = process.env.GITHUB_TOKEN;
-  const octokit = github.getOctokit(token);
+    const token = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(token);
 
-  const { owner, repo } = github.context.repo;
-  const { sha } = github.context.payload.head_commit || github.context.payload.commits[0];
+    const { owner, repo } = github.context.repo;
+    const { sha } = github.context.payload.head_commit || github.context.payload.commits[0];
 
-  const compareCommitsResponse = await octokit.repos.compareCommits({
-    owner,
-    repo,
-    base: sha,
-    head: `${sha}^`
-  });
+    const compareCommitsResponse = await octokit.repos.compareCommits({
+        owner,
+        repo,
+        base: sha,
+        head: `${sha}^`
+    });
 
-  return compareCommitsResponse.data.files.map(file => file.filename);
+    return compareCommitsResponse.data.files.map(file => file.filename);
 }
 
 run();
